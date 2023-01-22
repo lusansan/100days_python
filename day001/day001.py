9da46274a674c5ed6cd7b8ac516097dbd21fa360
loker86 <loker_yu@outlook.com>
2023-01-22 20:28:50 +0800

    day001

--- day001/day001.py
+++ day001/day001.py
@@ -1,1 +1,4 @@
-print("hello world")
+print("Welcome to The Band Name Generator!")
+city_name = input("What city did you grow up in?\n")
+pet_name = input("What is the name of your first pet?\n")
+print("Your band name is " + city_name + " " + pet_name)
---
+++ day001/ex01.py
@@ -0,0 +1,5 @@
+# Write your code below this line
+
+print('Day 1 - Python Print Function')
+print('The fuction is delared like this:')
+print("print('what to print')")
