413fe9a260775040f9dc79ff6419534a826d0cd5
loker86 <loker_yu@outlook.com>
2023-01-22 23:30:35 +0800

    day003

--- day003/ex01.py
+++ day003/ex01.py
@@ -1,7 +1,6 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+number = int(input("Which number do you want to check? "))
 
-if height < 120:
-    print("Sorry, you have to grow taller before you can ride.")
+if number % 2 == 0:
+    print("This is an even number.")
 else:
-    print("You can ride the rollercoaster!")
+    print("This is an odds number.")
--- day003/ex02.py
+++ day003/ex02.py
@@ -1,6 +1,17 @@
-number = int(input("Which number do you want to check? "))
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg: "))
 
-if number % 2 == 0:
-    print("This is an even number.")
+bmi = weight / height ** 2
+
+print("Your BMI is {:.1f}".format(bmi))
+
+if bmi < 18.5:
+    print("You are under weight.")
+elif bmi < 25:
+    print("You have a normal weight.")
+elif bmi < 30:
+    print("You are overweight.")
+elif bmi < 35:
+    print("You are obese.")
 else:
-    print("This is an odds number.")
+    print("You are clinically obese.")
