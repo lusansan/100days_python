506e7033506a35ff719f9cc810c6fd3d0407689f
lusansan <loker.yu@gmail.com>
2023-01-23 22:42:55 +0800

    day003

--- day003/ex00.py
+++ day003/ex00.py
@@ -1,5 +1,6 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height < 120:
     print("Sorry, you have to grow taller before you can ride.")
@@ -7,8 +8,18 @@ else:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
     if age < 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Child tickets are $5.")
     elif age <= 18 :
-        print("Please pay $7.")
+        bill = 7
+        print("Youth tickets are $7.")
     else :
-        print("Please pay $12.")
+        bill = 12
+        print("Adult tickets are $12.")
+    photo_wants = input("Do you want a photo taken?(Y or N.) ")
+
+    if photo_wants == "Y":
+        bill += 3
+    
+    print(f"Your final bill is {bill}")
+
---
+++ day003/ex03.py
@@ -0,0 +1,10 @@
+year = int(input("Which year do you want to check?\n"))
+
+if year % 400 == 0:
+    print(f"{year} is a leap year!")
+elif year % 100 == 0:
+    print(f"{year} is not a leap year!")
+elif year % 4 == 0:
+    print(f"{year} is a leap year!")
+else:
+    print(f"{year} is not a leap year!")
