124a1845ac54cf30d03586cbadfe2202fee7f511
lusansan <loker.yu@gmail.com>
2023-01-25 23:04:42 +0800

    day005

---
+++ day005/ex03.py
@@ -0,0 +1,13 @@
+sum_even = 0
+for i in range(2,101,2):
+    sum_even += i
+
+print(sum_even)
+
+
+# sum_even = 0
+# for i in range(1,101):
+#     if i % 2 == 0:
+#         sum_even += i
+
+# print(sum_even)
