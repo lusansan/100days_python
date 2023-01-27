346c44c1a38f5b29bbe3ebe6cef32251b65c1038
lusansan <loker.yu@gmail.com>
2023-01-27 23:53:15 +0800

    day007

--- day007/main.py
+++ day007/main.py
@@ -1,4 +1,17 @@
 from hangman_config import stages,logo2,logo3
 from hangman_words import word_list
+import random
 
-words_1 = ["ardvark", "baboon", "camel"]
+words_list_1 = ["ardvark", "baboon", "camel"]
+
+radom_int = random.randint(1,len(words_list_1))
+
+choose_word = words_list_1[radom_int-1]
+print(choose_word)
+
+word = input("Guess a letter.\n")
+
+if word in choose_word:
+    print("Right")
+else:
+    print("Wrong")
