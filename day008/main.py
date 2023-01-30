eadd7123bc8e564ac1a74edc0192066e14e73b4a
lusansan <loker.yu@gmail.com>
2023-01-30 22:53:23 +0800

    day008

--- day008/main.py
+++ day008/main.py
@@ -1,28 +1,28 @@
 from material import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-shift_num = 5
-
-pw = 'abc'
-
-def encode_letter(letter,shift_num):
-    letter_index = alphabet.index(letter)
-    letter_shift_index = (letter_index + shift_num) % 26
-    encode_letter = alphabet[letter_shift_index]
-
-
 def caesar(start_text, shift_amount, cipher_direction):
-    if cipher_direction == 'encode':
-        shift_text = []
-        for letter in start_text:
+    if cipher_direction == 'decode':
+        shift_amount = -1 * shift_amount
+    cipher_text = ""
+    for letter in start_text:
+        if letter in alphabet:
             letter_index = alphabet.index(letter)
-            letter_shift_index = (letter_index + shift_num) % 26
-            shift_text.append(alphabet[letter_shift_index])
+            letter_shift_index = (letter_index + shift_amount + 26) % 26
+            cipher_text += alphabet[letter_shift_index]
+        else:
+            cipher_text += letter
+    print(f"Here's the {cipher_direction}d result: {cipher_text}\n")
 
 
 run = True
 while run:
-  print(logo)
-  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-  text = input("Type your message:\n").lower()
-  shift = int(input("Type the shift number:\n"))
+    print(logo)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    cipher_text = caesar(start_text = text, shift_amount = shift, cipher_direction = direction)
+    choice = input("Do you want to run this program again?\nType 'Yes' or 'No': ").lower()
+    if choice == 'no':
+        run = False
+        print("Goodbye.")
