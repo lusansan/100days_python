c5e58de954cc62d8ce85c5fb7dfa78f064c15f13
lusansan <loker.yu@gmail.com>
2023-02-02 22:52:22 +0800

    day010

--- day010/main.py
+++ day010/main.py
@@ -1,43 +1,29 @@
-
-num_1 = input("What is the first number?\n")
-
-print("+\n-\n*\n/")
-
-def calculate(num_1,num_2,operation):
-    return eval(str(num_1) + operation + str(num_2))
-
-print(f"Type 'y' to continue calculating with {answer}, type 'n' to exit or type 'new' for a brand new calculation")
-
-continue_calc = input("Type y/n/new: ")
-
-operation = input("Pick on operation:\n") 
-
-num_2 = input("What is the next number?\n")
-
+from material import logo
 
 
 def cal():
+    print(logo)
+    num_1 = int(input("What is the first number?\n"))
     run = True 
     while run:
-        num_1 = input("What is the first number?\n")
         print("+\n-\n*\n/")
         operation = input("Pick on operation:\n") 
-        num_2 = input("What is the next number?\n")
-        ret = calculate(num_1, num_2, operation)
+        num_2 = int(input("What is the next number?\n"))
+        ret = eval(str(num_1) + operation + str(num_2))
 
         print(f"Type 'y' to continue calculating with {ret}, type 'n' to exit or type 'new' for a brand new calculation")
         continue_calc = input("Type y/n/new: ").lower()
         if continue_calc == 'y':
-            num_2 = input("What is the next number?\n")
-            print("+\n-\n*\n/")
-            operation = input("Pick on operation:\n") 
-            ret = calculate(num_1, num_2, operation)
-            print(f"Type 'y' to continue calculating with {ret}, type 'n' to exit or type 'new' for a brand new calculation")
+            run = True
+            num_1 = ret
         elif continue_calc == 'new':
             cal()
         elif continue_calc == 'n':
             run = False
+            print("\nGoodbye.")
         else:
-            print
-
+            print("Invalid response.")
+            run = False
+            print("\nGoodbye.")
+cal()
 
